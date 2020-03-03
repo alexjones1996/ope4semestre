3a48f7cf41b2838d1148a1c92c72aac15abad017
Alex Jones <alex.silva@aluno.faculdadeimpacta.com.br>
2020-03-03 15:21:38 -0300

    Correção da URL - PrototipoSamuel

--- Prototipo/PrototipoSamuel/ALMSolutions/agenda/urls.py
+++ Prototipo/PrototipoSamuel/ALMSolutions/agenda/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from agenda import views 
 urlpatterns = [
     path('', views.index),
-    path('eventos', views.home),
+    path('eventos/', views.home),
     path('agenda/', views.agenda),
 ]
