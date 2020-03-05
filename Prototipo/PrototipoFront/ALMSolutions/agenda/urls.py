8ef7d4e3279141303d12413830e484770151cace
Alex Jones <alex.silva@aluno.faculdadeimpacta.com.br>
2020-03-05 17:43:49 -0300

    unificação de prototipos

--- Prototipo/PrototipoFront/ALMSolutions/agenda/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from django.urls import path
-from agenda.views import home, agenda, api
-
-urlpatterns = [
-    path('', home),
-    path('api/', api),
-    path('agenda/', agenda),
-]
